26c27f420a750596b9580dcbc8a3ffca4d88fea0
aoba43 <emerson8096@gmail.com>
2023-11-08 17:04:14 -0300

    CSS modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Emerson" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
