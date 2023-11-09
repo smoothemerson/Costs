62d5660d3953d1351adf478047fb7f0a360426af
aoba43 <emerson8096@gmail.com>
2023-11-09 10:51:47 -0300

    Using React Fragments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
       <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />
+      <List />
     </div>
   );
 }
