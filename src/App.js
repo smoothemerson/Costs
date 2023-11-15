e1cf9c0e10d73fe36352a89020407f5a765993c0
aoba43 <emerson8096@gmail.com>
2023-11-15 14:47:16 -0300

    Implementing React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { useState } from 'react';
-
-import './App.css'
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Empresa from './pages/Empresa'
+import Navbar from './components/layout/Navbar'
+import Footer from "./components/layout/Footer"
 
 function App() {
 
-  const [nome, setNome] = useState()
-  
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome} />
-      <Saudacao nome={nome} />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} ></Route>
+        <Route path="/empresa" element={<Empresa />} ></Route>
+        <Route path="/contato" element={<Contato />} ></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
