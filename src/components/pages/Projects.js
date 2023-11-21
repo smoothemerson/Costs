9228332bf6bb640c61fd8e04f627c36915c31807
aoba43 <emerson8096@gmail.com>
2023-11-21 15:19:05 -0300

    Rescuing projects from the database

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,37 @@
 import { useLocation } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+
 import Message from "../layout/Mensage";
 import Container from "../layout/Container";
 
 import styles from "./Projects.module.css";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -22,7 +42,16 @@ function Projects() {
       {message && <Message type="success" msg={message} />}
 
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category?.name}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );
