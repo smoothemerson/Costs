e6af36f9a57c9d16ee1fb9020158ad538dba757d
aoba43 <emerson8096@gmail.com>
2023-11-23 09:17:45 -0300

    Data edit page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 
 function App() {
   return (
@@ -16,10 +17,11 @@ function App() {
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/projects" element={<Projects />} />
-          <Route exact path="/company" element={<Company />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
